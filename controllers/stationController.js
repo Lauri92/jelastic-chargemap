6bd2d3766539552868032b67a77fb4a9ef1390e3
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-03-29 13:22:17 +0300

    stations by geojson

--- controllers/stationController.js
+++ controllers/stationController.js
@@ -1,37 +1,45 @@
 'use strict';
 
 import station from '../models/stationModel.js';
+import {rectangleBounds} from '../utils/rectangleBounds.js';
 
 const station_list_get = async (req, res) => {
-  const resultLimit = req.query.limit || 10
-  res.json(
-      await station.find().populate([
-        {
-          path: 'Connections',
-          populate: {path: 'ConnectionTypeID'},
-        }, {
-          path: 'Connections',
-          populate: {path: 'LevelID'},
-        }, {
-          path: 'Connections',
-          populate: {path: 'CurrentTypeID'},
-        }],
-      ).limit(resultLimit));
+  try {
+    const resultLimit = req.query.limit || 10;
+    res.json(
+        await station.find().populate(stationPopulationOptions).limit(resultLimit));
+  } catch (e) {
+    res.status(400).send('Something went wrong');
+  }
 };
 
 const station_get = async (req, res) => {
-  res.json(await station.findById(req.params.id).populate([
-    {
-      path: 'Connections',
-      populate: {path: 'ConnectionTypeID'},
-    }, {
-      path: 'Connections',
-      populate: {path: 'LevelID'},
-    }, {
-      path: 'Connections',
-      populate: {path: 'CurrentTypeID'},
-    }],
-  ));
+  try {
+    res.json(await station.findById(req.params.id).populate(stationPopulationOptions));
+  } catch (e) {
+    res.status(400).send('Something went wrong');
+  }
+};
+
+const station_list_get_by_area = async (req, res) => {
+  try {
+    if (req.query.topRight && req.query.bottomLeft) {
+      const topRight = JSON.parse(req.query.topRight);
+      const bottomLeft = JSON.parse(req.query.bottomLeft);
+      const polygon = rectangleBounds(topRight, bottomLeft);
+
+      const stations = await station.find().
+          populate(stationPopulationOptions).
+          where('Location').
+          within(polygon);
+
+      res.send(stations);
+    } else {
+      res.status(400).send('Specify coordinates');
+    }
+  } catch (e) {
+    res.status(400).send('Something went wrong');
+  }
 };
 
 const station_post = async (req, res) => {
@@ -49,9 +57,23 @@ const station_post = async (req, res) => {
 const station_patch = async (req, res) => {
 };
 
+const stationPopulationOptions = [
+
+  {
+    path: 'Connections',
+    populate: {path: 'ConnectionTypeID'},
+  }, {
+    path: 'Connections',
+    populate: {path: 'LevelID'},
+  }, {
+    path: 'Connections',
+    populate: {path: 'CurrentTypeID'},
+  }];
+
 export {
   station_list_get,
   station_get,
+  station_list_get_by_area,
   station_post,
   station_patch,
 };
--- routes/stationRoute.js
+++ routes/stationRoute.js
@@ -5,7 +5,7 @@ import {
   station_list_get,
   station_get,
   station_post,
-  station_patch,
+  station_patch, station_list_get_by_area,
 } from '../controllers/stationController.js';
 import passport from '../utils/pass.js';
 
@@ -15,9 +15,10 @@ router.route('/').
     get(station_list_get).
     post(station_post, passport.authenticate('jwt', {session: false}));
 
+router.route('/area').get(station_list_get_by_area);
+
 router.route('/:id').
     get(station_get).
-    patch(station_patch)
-;
+    patch(station_patch);
 
 export default router;
